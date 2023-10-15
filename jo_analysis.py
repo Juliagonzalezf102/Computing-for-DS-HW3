9356bb1a4d221b75df0f39e546878a01561046f5
jnossa <joaquin.ossa@bse.eu>
2023-10-15 17:26:55 +0200

    Created library with functions and used them

---
+++ jo_analysis.py
@@ -0,0 +1,31 @@
+#%%
+import pandas as pd
+import numpy as np
+from  jolibhw3 import module1
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+
+data = pd.read_csv('sample_diabetes_mellitus_data.csv')
+data.head()
+
+#%%
+# Clean data
+data = module1.remove_nan(data, ['age', 'gender', 'ethnicity'])
+data = module1.fillmean_nan(data, ['height', 'weight'])
+
+#%%
+# Calculating the mean diabetes_mellitus for each ethnicity
+mean_diabetes_mellitus = data.groupby('ethnicity')['diabetes_mellitus'].mean()
+data['ethn_value'] = data['ethnicity'].map(mean_diabetes_mellitus)
+
+gender_map = {'M':0, 'F':1}
+data = module1.mapping(data, 'gender', gender_map)
+
+#%%
+# Predicting values
+feat_col = ['age', 'height', 'weight', 'aids', 'cirrhosis', 'hepatic_failure',
+'immunosuppression', 'leukemia', 'lymphoma', 'solid_tumor_with_metastasis']
+data = module1.predict_model(data, feat_col, 'diabetes_mellitus')
+
+# %%
