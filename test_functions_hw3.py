ab6aa39bb06c434b0ee6b149ac9043971f9c66f3
Guillem Mirabent Rubinat <guillem.mirabent@bse.eu>
2023-11-01 11:13:43 +0100

    update to make all the tests pass

--- test_functions_hw3.py
+++ test_functions_hw3.py
@@ -1,6 +1,5 @@
-sys.path.append(
-    r'"C:\Users\julia\OneDrive\Documentos\GitHub\Computing-for-DS-HW3"')
-
+# %%
+import datetime as dt
 from hw3_updated import sum_general_int_list
 from hw3_updated import get_day_month_year
 from hw3_updated import compute_distance
@@ -21,7 +20,7 @@ class countsimba(unittest.TestCase):
 
     def test_count_simba_many_values(self):
         input_list = ["Hello Simba",
-                      "Good Morning, Simba", "Good Night, Simba"]
+                    "Good Morning, Simba", "Good Night, Simba"]
         output = count_simba(input_list)
         expected_output = 3
         self.assertEqual(output, expected_output)
@@ -34,7 +33,7 @@ class countsimba(unittest.TestCase):
 
     def test_count_simba_one_value(self):
         input_list = ["Hello Simba",
-                      "Good Morning", "Good Night"]
+                    "Good Morning", "Good Night"]
         output = count_simba(input_list)
         expected_output = 1
         self.assertEqual(output, expected_output)
@@ -45,6 +44,7 @@ class countsimba(unittest.TestCase):
 class Getdaymonthyear(unittest.TestCase):
 
     def test_get_day_month_year(self):
+
         input_list = [
             dt.date(2022, 1, 1),
             dt.date(2022, 1, 2),
@@ -52,18 +52,26 @@ class Getdaymonthyear(unittest.TestCase):
             dt.date(2022, 8, 13)
         ]
         output = get_day_month_year(input_list)
-        expected_output = pd.dataframe({'day': [1, 2, 3, 13], 'month': [1, 1, 1, 8], 'year': [
-                                       2022, 2022, 2022, 2022]}, index=['date', 'date', 'date'])
-        self.assertEqual(output, expected_output)
+        expected_output = pd.DataFrame({'day': [1, 2, 3, 13], 'month': [1, 1, 1, 8], 'year': [
+                                    2022, 2022, 2022, 2022]}, index=['date', 'date', 'date', 'date'])
+        self.assertTrue(expected_output.equals(output))
+
+    def test_get_day_month_year_empyt_list(self):
+
+        input_list = []
+        output = get_day_month_year(input_list)
+        expected_output = pd.DataFrame(
+            columns=['day', 'month', 'year'], index=[])
+        self.assertTrue(expected_output.equals(output))
 
 
 # TEST FUNCTION 3
 class Computedistance(unittest.TestCase):
     def test_compute_distance(self):
-        input_list = [((41.23, 23.5), (41.5, 23.4)),
-                      ((52.38, 20.1), (52.3, 17.8))]
+        input_list = [((39.51, 21.3), (25.3, 55.67)),
+                    ((32.31, 2.12), (52.38, 19.23))]
         output = compute_distance(input_list)
-        expected_output = "The distances are 31.13 and 157.01 km"
+        expected_output = "The distances are 3567.42 and 2622.91 km"
         self.assertEqual(output, expected_output)
 
     def test_compute_distance_0(self):
@@ -73,7 +81,8 @@ class Computedistance(unittest.TestCase):
         self.assertEqual(output, expected_output)
 
     def test_compute_distance_missing(self):
-        input_list = [((0, 0)), ((0, 0), (0, 0))]
+        input_list = [((39.51, 21.3), (25.3, 55.67)),
+                    ((32.31, 2.12))]
         self.assertRaises(ValueError)
 
 
@@ -82,7 +91,7 @@ class Sumgeneralintlist(unittest.TestCase):
     def test_general_int_list(self):
         input_list = [[1, 2, 3], 4, [5, [6, 7], 8, 9]]
         output = sum_general_int_list(input_list)
-        expected_output = 40
+        expected_output = 45
         self.assertEqual(output, expected_output)
 
     def test_general_int_list_no_values(self):
